e1e8092d7f4bb3be27b7bd0d7e06a2a4655b28cf
Sholanki <63105386+Sholanki@users.noreply.github.com>
2020-09-10 22:23:31 +0530

    Add files via upload

--- src/admin/util.js
+++ src/admin/util.js
@@ -2,10 +2,34 @@ import helpers from '../_data/helpers';
 import dateFilter from '../filters/date-filter';
 import markdownFilter from '../filters/markdown-filter';
 import w3DateFilter from '../filters/w3-date-filter';
+import tweetFilter from '../filters/tweetFilter';
 
 export {
   helpers,
   dateFilter,
   markdownFilter,
   w3DateFilter,
+  tweetFilter,
 };
+
+const exposed = {};
+if (location.search) {
+  var a = document.createElement("a");
+  a.href = location.href;
+  a.search = "";
+  history.replaceState(null, null, a.href);
+}
+
+function expose(name, fn) {
+  exposed[name] = fn;
+}
+    function tweet_(url) {
+	  open(
+		"https://twitter.com/intent/tweet?url=" + encodeURIComponent(url),
+		"_blank"
+	  );
+	}
+	function tweet(anchor) {
+	  tweet_(anchor.getAttribute("href"));
+	}
+	expose("tweet", tweet);
